e753241f3210038f467f94b855b64901252814ac
DIVYANG_CHOUDHARY <divyangchoudhary75@gmail.com>
2023-03-10 10:41:27 +0530

    updating Js file

--- Js/index.js
+++ Js/index.js
@@ -214,3 +214,11 @@ console.log(d)
 
 let myDate= new Date();
 console.log(myDate)
+console.log(myDate.getTime());
+console.log(myDate.getFullYear());
+console.log(myDate.getDay());
+console.log(myDate.getMinutes());
+console.log(myDate.getHours());
+
+// DOM Manipulation
+document
